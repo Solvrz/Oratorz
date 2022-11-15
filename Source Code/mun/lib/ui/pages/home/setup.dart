bd6185e21d874655ce8560acc79e657e103627f4
AdityaTaggar05 <aditya05.mgg@gmail.com>
2022-11-15 23:33:06 +0530

    Added Country Information

--- Source Code/mun/lib/ui/pages/home/setup.dart
+++ Source Code/mun/lib/ui/pages/home/setup.dart
@@ -1,49 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '/config/country_info.dart';
+
 class SetupPage extends StatelessWidget {
   const SetupPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    List<Map<String, String>> data = [
-      {
-        "name": "United States of America",
-        "flag": "",
-      },
-      {
-        "name": "India",
-        "flag": "",
-      },
-      {
-        "name": "United Kingdoms",
-        "flag": "",
-      },
-      {
-        "name": "France",
-        "flag": "",
-      },
-      {
-        "name": "China",
-        "flag": "",
-      },
-      {
-        "name": "UAE",
-        "flag": "",
-      },
-      {
-        "name": "Canada",
-        "flag": "",
-      },
-      {
-        "name": "Germany",
-        "flag": "",
-      },
-      {
-        "name": "Japan",
-        "flag": "",
-      },
-    ];
-
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -60,119 +23,139 @@ class SetupPage extends StatelessWidget {
               Expanded(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(16),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Load Committee",
-                              style: Theme.of(context).textTheme.headline5,
-                            ),
-                            const SizedBox(height: 18),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: TextButton(
-                                    child: const Text("From File"),
-                                    onPressed: () {},
-                                  ),
-                                ),
-                                const SizedBox(width: 20),
-                                Expanded(
-                                  child: TextButton(
-                                    child: const Text("From Template"),
-                                    onPressed: () {},
-                                  ),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 12),
-                    Expanded(
-                      child: Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(16),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Set Up New Committee",
-                                style: Theme.of(context).textTheme.headline5,
-                              ),
-                              Expanded(
-                                child: Padding(
-                                  padding:
-                                      const EdgeInsets.only(top: 12, left: 12),
-                                  child: Column(
-                                    children: [
-                                      Text(
-                                        "UN Member States",
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .headline6,
-                                      ),
-                                      Expanded(
-                                        child: Scrollbar(
-                                          child: ListView.separated(
-                                            itemBuilder: (context, index) =>
-                                                ListTile(
-                                              leading: CircleAvatar(
-                                                radius: 20,
-                                                backgroundColor:
-                                                    Colors.grey.shade200,
-                                              ),
-                                              title: Text(
-                                                data[index]["name"]!,
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .headline6,
-                                              ),
-                                            ),
-                                            separatorBuilder:
-                                                (context, index) =>
-                                                    const Divider(),
-                                            itemCount: data.length,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 36),
+                  children: const [
+                    LoadCommitteeCard(),
+                    SizedBox(height: 12),
+                    Expanded(child: NewCommitteeCard(data: COUNTRIES)),
+                    SizedBox(height: 36),
                   ],
                 ),
               ),
               const SizedBox(width: 32),
-              Expanded(
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Your Committee",
-                          style: Theme.of(context).textTheme.headline5,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+              const Expanded(child: CommitteeCard()),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class CommitteeCard extends StatelessWidget {
+  const CommitteeCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Your Committee",
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NewCommitteeCard extends StatelessWidget {
+  const NewCommitteeCard({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  final List<Map<String, String>> data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Set Up New Committee",
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 12, left: 12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "UN Member States",
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    Expanded(
+                      child: ListView.separated(
+                        itemBuilder: (context, index) => ListTile(
+                          leading: CircleAvatar(
+                            radius: 20,
+                            child: Image.asset(data[index]["flag"]!),
+                          ),
+                          title: Text(
+                            data[index]["name"]!,
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                        ),
+                        separatorBuilder: (context, index) => const Divider(),
+                        itemCount: data.length,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoadCommitteeCard extends StatelessWidget {
+  const LoadCommitteeCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Load Committee",
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            const SizedBox(height: 18),
+            Row(
+              children: [
+                Expanded(
+                  child: TextButton(
+                    child: const Text("From File"),
+                    onPressed: () {},
+                  ),
+                ),
+                const SizedBox(width: 20),
+                Expanded(
+                  child: TextButton(
+                    child: const Text("From Template"),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
