ab2d78365dee2a337784067932e1dbc68daebdc7
AdityaTaggar05 <aditya05.mgg@gmail.com>
2025-08-26 15:25:11 +0530

    [fix] Fetching previous day scorecards

--- Source Code/website/lib/ui/pages/committee/widgets/day_picker.dart
+++ Source Code/website/lib/ui/pages/committee/widgets/day_picker.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '/services/cloud_storage.dart';
+import '/tools/controllers/comittee/autosave.dart';
 import '/tools/controllers/comittee/committee.dart';
 
 class DayPicker extends StatelessWidget {
   const DayPicker({super.key});
+
+  void fetchNewScorecard() {
+    final AutoSaveController saveController = Get.find<AutoSaveController>();
+    final CommitteeController committeeController =
+        Get.find<CommitteeController>();
+
+    if (saveController.timers["scorecard"] != null) {
+      saveController.timers["scorecard"]?.cancel();
+      CloudStorage.saveScorecard(
+        data: committeeController.committee.scorecard!.toJson(),
+      );
+    }
+
+    committeeController.refetch = true;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +42,8 @@ class DayPicker extends StatelessWidget {
               controller.selectedDay.value =
                   controller.committee.days.length - 1;
             }
+
+            fetchNewScorecard();
           },
           child: const Icon(Icons.arrow_left_rounded, color: Colors.white),
         ),
@@ -52,6 +71,8 @@ class DayPicker extends StatelessWidget {
                 controller.committee.days.length - 1) {
               controller.selectedDay.value = 0;
             }
+
+            fetchNewScorecard();
           },
           child: const Icon(Icons.arrow_right_rounded, color: Colors.white),
         ),
@@ -60,8 +81,10 @@ class DayPicker extends StatelessWidget {
           hoverColor: Colors.white10,
           focusColor: Colors.transparent,
           highlightColor: Colors.transparent,
-          onTap: () =>
-              controller.selectedDay.value = controller.committee.currDay,
+          onTap: () {
+            controller.selectedDay.value = controller.committee.currDay;
+            fetchNewScorecard();
+          },
           child: const Padding(
             padding: EdgeInsets.all(2),
             child: Icon(
