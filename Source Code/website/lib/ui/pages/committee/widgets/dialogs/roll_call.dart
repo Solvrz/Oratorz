eedca8977384bba2a6cac4328735c9ae081d575d
AdityaTaggar05 <aditya05.mgg@gmail.com>
2025-08-28 17:35:25 +0530

    [fix] Read only for previous day roll call

--- Source Code/website/lib/ui/pages/committee/widgets/dialogs/roll_call.dart
+++ Source Code/website/lib/ui/pages/committee/widgets/dialogs/roll_call.dart
@@ -43,63 +43,65 @@ class _RollCallDialogState extends State<RollCallDialog> {
 
             return Column(
               children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: RoundedButton(
-                        style: RoundedButtonStyle.border,
-                        onPressed: updating
-                            ? null
-                            : () {
-                                controller.setAllPresent();
-                                controller.update();
-                              },
-                        child: Text(
-                          "SET ALL PRESENT",
-                          style: context.textTheme.bodyLarge,
+                if (!controller.readOnly) ...[
+                  Row(
+                    children: [
+                      Expanded(
+                        child: RoundedButton(
+                          style: RoundedButtonStyle.border,
+                          onPressed: updating
+                              ? null
+                              : () {
+                                  controller.setAllPresent();
+                                  controller.update();
+                                },
+                          child: Text(
+                            "SET ALL PRESENT",
+                            style: context.textTheme.bodyLarge,
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(width: 16),
-                    Expanded(
-                      child: RoundedButton(
-                        style: RoundedButtonStyle.border,
-                        onPressed: updating
-                            ? null
-                            : () {
-                                controller.setAllAbsent();
-                                controller.update();
-                              },
-                        child: Text(
-                          "SET ALL ABSENT",
-                          style: context.textTheme.bodyLarge,
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: RoundedButton(
+                          style: RoundedButtonStyle.border,
+                          onPressed: updating
+                              ? null
+                              : () {
+                                  controller.setAllAbsent();
+                                  controller.update();
+                                },
+                          child: Text(
+                            "SET ALL ABSENT",
+                            style: context.textTheme.bodyLarge,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Row(
-                  children: [
-                    Expanded(
-                      child: RoundedButton(
-                        style: RoundedButtonStyle.border,
-                        onPressed: updating
-                            ? null
-                            : () {
-                                controller.setAllPresentAndVoting();
-                                controller.update();
-                              },
-                        child: Text(
-                          "SET ALL PRESENT & VOTING",
-                          textAlign: TextAlign.center,
-                          style: context.textTheme.bodyLarge,
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: RoundedButton(
+                          style: RoundedButtonStyle.border,
+                          onPressed: updating
+                              ? null
+                              : () {
+                                  controller.setAllPresentAndVoting();
+                                  controller.update();
+                                },
+                          child: Text(
+                            "SET ALL PRESENT & VOTING",
+                            textAlign: TextAlign.center,
+                            style: context.textTheme.bodyLarge,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 16),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                ],
                 Expanded(
                   flex: 3,
                   child: ListView.builder(
@@ -123,7 +125,7 @@ class _RollCallDialogState extends State<RollCallDialog> {
                                       ? RoundedButtonStyle.fill
                                       : RoundedButtonStyle.border,
                                   color: context.theme.colorScheme.tertiary,
-                                  onPressed: updating
+                                  onPressed: updating || controller.readOnly
                                       ? null
                                       : () {
                                           controller.setRollCall(
@@ -141,7 +143,7 @@ class _RollCallDialogState extends State<RollCallDialog> {
                                       ? RoundedButtonStyle.fill
                                       : RoundedButtonStyle.border,
                                   color: Colors.amber.shade400,
-                                  onPressed: updating
+                                  onPressed: updating || controller.readOnly
                                       ? null
                                       : () {
                                           controller.setRollCall(
@@ -159,7 +161,7 @@ class _RollCallDialogState extends State<RollCallDialog> {
                                       ? RoundedButtonStyle.fill
                                       : RoundedButtonStyle.border,
                                   color: Colors.red.shade400,
-                                  onPressed: updating
+                                  onPressed: updating || controller.readOnly
                                       ? null
                                       : () {
                                           controller.setRollCall(
@@ -185,24 +187,25 @@ class _RollCallDialogState extends State<RollCallDialog> {
         ),
       ),
       actions: [
-        SizedBox(
-          width: context.width / 3,
-          child: RoundedButton(
-            color: updating ? Colors.blueGrey : null,
-            onPressed: updating
-                ? null
-                : () async {
-                    setState(() => updating = true);
-                    await CloudStorage.saveRollCall();
-                    setState(() => updating = false);
+        if (!controller.readOnly)
+          SizedBox(
+            width: context.width / 3,
+            child: RoundedButton(
+              color: updating ? Colors.blueGrey : null,
+              onPressed: updating
+                  ? null
+                  : () async {
+                      setState(() => updating = true);
+                      await CloudStorage.saveRollCall();
+                      setState(() => updating = false);
 
-                    if (context.mounted) {
-                      context.pop();
-                    }
-                  },
-            child: const Text("UPDATE"),
+                      if (context.mounted) {
+                        context.pop();
+                      }
+                    },
+              child: const Text("UPDATE"),
+            ),
           ),
-        ),
       ],
     );
   }
