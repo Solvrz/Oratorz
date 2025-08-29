7d6fe545f4d92e7dca69c877f5c3afb32e173fa8
AdityaTaggar05 <aditya05.mgg@gmail.com>
2025-08-29 14:32:27 +0530

    [fix] Caching of motions/votes not working due to missing GetBuilder

--- Source Code/website/lib/ui/pages/committee/motions/widgets/past_motions_card.dart
+++ Source Code/website/lib/ui/pages/committee/motions/widgets/past_motions_card.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 
 import '/services/cloud_storage.dart';
 import '/tools/controllers/comittee/motions.dart';
+import '../../../../../tools/controllers/comittee/committee.dart';
 import './motion_tile.dart';
 
 class PastMotionsCard extends StatelessWidget {
@@ -26,42 +27,46 @@ class PastMotionsCard extends StatelessWidget {
               ),
               const SizedBox(height: 10),
               Expanded(
-                child: FutureBuilder(
-                  future: CloudStorage.fetchPastMotions(),
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData) {
-                      return const Center(
-                        child: SizedBox(
-                          child: CircularProgressIndicator(
-                            color: Color(0xff2a313b),
+                child: GetBuilder<CommitteeController>(
+                  builder: (_) => FutureBuilder(
+                    future: CloudStorage.fetchPastMotions(),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return const Center(
+                          child: SizedBox(
+                            child: CircularProgressIndicator(
+                              color: Color(0xff2a313b),
+                            ),
                           ),
-                        ),
-                      );
-                    }
+                        );
+                      }
 
-                    final List<Map<String, dynamic>> pastMotions =
-                        controller.pastMotions.toList().sorted(
-                              (a, b) =>
-                                  a["timestamp"].compareTo(b["timestamp"]) * -1,
-                            );
+                      final List<Map<String, dynamic>> pastMotions = controller
+                          .pastMotions
+                          .toList()
+                          .sorted(
+                            (a, b) =>
+                                a["timestamp"].compareTo(b["timestamp"]) * -1,
+                          );
 
-                    if (pastMotions.isNotEmpty) {
-                      return ListView.builder(
-                        itemCount: pastMotions.length,
-                        itemBuilder: (_, index) => Padding(
-                          padding: EdgeInsets.only(
-                            bottom: index != pastMotions.length - 1 ? 20 : 0,
+                      if (pastMotions.isNotEmpty) {
+                        return ListView.builder(
+                          itemCount: pastMotions.length,
+                          itemBuilder: (_, index) => Padding(
+                            padding: EdgeInsets.only(
+                              bottom: index != pastMotions.length - 1 ? 20 : 0,
+                            ),
+                            child: MotionTile(motion: pastMotions[index]),
                           ),
-                          child: MotionTile(motion: pastMotions[index]),
-                        ),
-                      );
-                    } else {
-                      return Text(
-                        "No motions added yet.",
-                        style: context.textTheme.bodyLarge,
-                      );
-                    }
-                  },
+                        );
+                      } else {
+                        return Text(
+                          "No motions added yet.",
+                          style: context.textTheme.bodyLarge,
+                        );
+                      }
+                    },
+                  ),
                 ),
               ),
             ],
--- Source Code/website/lib/ui/pages/committee/vote/widgets/past_votes_card.dart
+++ Source Code/website/lib/ui/pages/committee/vote/widgets/past_votes_card.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '/services/cloud_storage.dart';
+import '/tools/controllers/comittee/committee.dart';
 import '/tools/controllers/comittee/vote.dart';
 import '/tools/extensions.dart';
 
@@ -27,65 +28,68 @@ class PastVotesCard extends StatelessWidget {
               ),
               const SizedBox(height: 12),
               Expanded(
-                child: FutureBuilder(
-                  future: CloudStorage.fetchPastVotes(),
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData) {
-                      return const Center(
-                        child: SizedBox(
-                          child: CircularProgressIndicator(
-                            color: Color(0xff2a313b),
-                          ),
-                        ),
-                      );
-                    }
-
-                    final List<Map<String, dynamic>> pastVotes =
-                        voteController.pastVotes.sorted(
-                      (a, b) => a["timestamp"].compareTo(b["timestamp"]) * -1,
-                    );
-
-                    return pastVotes.isNotEmpty
-                        ? ListView.separated(
-                            itemCount: pastVotes.length,
-                            separatorBuilder: (_, __) => Divider(
-                              height: 6,
-                              color: Colors.grey.shade400,
+                child: GetBuilder<CommitteeController>(
+                  builder: (_) => FutureBuilder(
+                    future: CloudStorage.fetchPastVotes(),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return const Center(
+                          child: SizedBox(
+                            child: CircularProgressIndicator(
+                              color: Color(0xff2a313b),
                             ),
-                            itemBuilder: (_, index) => ListTile(
-                              title: RichText(
-                                text: TextSpan(
-                                  text: "Topic: ",
-                                  style: context.textTheme.bodyLarge?.copyWith(
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                  children: [
-                                    TextSpan(
-                                      text: pastVotes[index]["topic"],
-                                      style: context.textTheme.bodySmall,
+                          ),
+                        );
+                      }
+
+                      final List<Map<String, dynamic>> pastVotes =
+                          voteController.pastVotes.sorted(
+                        (a, b) => a["timestamp"].compareTo(b["timestamp"]) * -1,
+                      );
+
+                      return pastVotes.isNotEmpty
+                          ? ListView.separated(
+                              itemCount: pastVotes.length,
+                              separatorBuilder: (_, __) => Divider(
+                                height: 6,
+                                color: Colors.grey.shade400,
+                              ),
+                              itemBuilder: (_, index) => ListTile(
+                                title: RichText(
+                                  text: TextSpan(
+                                    text: "Topic: ",
+                                    style:
+                                        context.textTheme.bodyLarge?.copyWith(
+                                      fontWeight: FontWeight.bold,
                                     ),
-                                  ],
+                                    children: [
+                                      TextSpan(
+                                        text: pastVotes[index]["topic"],
+                                        style: context.textTheme.bodySmall,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                subtitle: Text(
+                                  Timestamp.fromMillisecondsSinceEpoch(
+                                    pastVotes[index]["timestamp"],
+                                  ).toDate().to12Hour,
+                                  style: context.textTheme.bodySmall,
+                                ),
+                                trailing: CircleAvatar(
+                                  radius: 5,
+                                  backgroundColor: pastVotes[index]["result"]
+                                      ? Colors.green
+                                      : Colors.red,
                                 ),
                               ),
-                              subtitle: Text(
-                                Timestamp.fromMillisecondsSinceEpoch(
-                                  pastVotes[index]["timestamp"],
-                                ).toDate().to12Hour,
-                                style: context.textTheme.bodySmall,
-                              ),
-                              trailing: CircleAvatar(
-                                radius: 5,
-                                backgroundColor: pastVotes[index]["result"]
-                                    ? Colors.green
-                                    : Colors.red,
-                              ),
-                            ),
-                          )
-                        : Text(
-                            "No motions added yet.",
-                            style: context.textTheme.bodyLarge,
-                          );
-                  },
+                            )
+                          : Text(
+                              "No motions added yet.",
+                              style: context.textTheme.bodyLarge,
+                            );
+                    },
+                  ),
                 ),
               ),
             ],
